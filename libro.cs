54a7a40410f5b42fca8e094c83189c3e70e06bd9
vreverberi <vreverberi@LAPTOP-4RCDH15P>
2026-09-13 17:41:36 -0300

    Add project files.

---
+++ libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_ComA_Grupo8
+{
+    internal class Libro
+    {
+        private string titulo;
+        private string autor;
+        private string editorial;
+
+        public Libro(string titulo, string autor, string editorial)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.editorial = editorial;
+        }
+
+        public string getTitulo()
+        {
+            return titulo;
+        }
+
+        public override string ToString()
+        {
+            return "Titulo: " + titulo + " Autor: " + autor + " Editorial: " + editorial;
+        }
+    }
+}
